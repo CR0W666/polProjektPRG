5692db8003f7e12f557d99b2e8daca97dc109f90
asqscdhcp666 <lukas.petracek@educanet.cz>
2020-01-13 22:18:35 +0100

    formatted

--- Rozvrh.java
+++ Rozvrh.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
+
 class Rozvrh {
     public static void main(String[] args) {
-        String rozvrh[][] = {
-            {" ", "HW", "HW", "NET", "NET", "PCV", "PCV", " ", " ANJ", "ANJ", " "},
-            {" ", "MAT", "ANJ", "PRG", "PRG", "CJL", "MAT", " ", " ", " ", " "},
-            {" ", "TEV", "TEV", "CJL", "FYZ", "ZMU", "MDO", "ANJ", " ", " ", " "},
-            {" ", "ZMU", "ZMU", "OPS", "OPS", "WEB", "WEB", " ", " ", " ", " "},
-            {" ", "POP", "POP", "OVS", "FYZ", "MAT", "MAT", " ", " ", " ", " "},
-        };
-        String dny[] = {"pondeli", "utery", "stredu", "ctvrtek", "patek"};
+        String rozvrh[][] = { { " ", "HW", "HW", "NET", "NET", "PCV", "PCV", " ", " ANJ", "ANJ", " " },
+                { " ", "MAT", "ANJ", "PRG", "PRG", "CJL", "MAT", " ", " ", " ", " " },
+                { " ", "TEV", "TEV", "CJL", "FYZ", "ZMU", "MDO", "ANJ", " ", " ", " " },
+                { " ", "ZMU", "ZMU", "OPS", "OPS", "WEB", "WEB", " ", " ", " ", " " },
+                { " ", "POP", "POP", "OVS", "FYZ", "MAT", "MAT", " ", " ", " ", " " }, };
+        String dny[] = { "pondeli", "utery", "stredu", "ctvrtek", "patek" };
         Scanner sc = new Scanner(System.in);
         String name = "";
         int nultaH = 0;
         int pocetHVDnu = 0;
 
-        
         System.out.println("Vitej v programu, který ti rekne, kdy mas hodiny programovani, Napis sve krestni jmeno");
         name = sc.nextLine();
         sc.reset();
-        if(name.equals("test")) {
+        if (name.equals("test")) {
 
             test(name);
             System.out.println("test mode");
@@ -30,35 +28,35 @@ class Rozvrh {
                 }
             }
             user(name, dny, nultaH, rozvrh, sc, pocetHVDnu);
-                
+
         }
         searchForPrg(rozvrh, dny, name);
-        
 
     }
+
     public static void test(String name) {
-        //rozvrh = testRozvrh();
-        
+        // rozvrh = testRozvrh();
+
         name = "Lukas";
     }
 
     // public static String[][] testRozvrh() {
-    //     String rozvrh[][] = {
-    //         {" ", "HW", "HW", "NET", "NET", "PCV", "PCV", " ", " ANJ", "ANJ", " "},
-    //         {" ", "MAT", "ANJ", "PRG", "PRG", "CJL", "MAT", " ", " ", " ", " "},
-    //         {" ", "TEV", "TEV", "CJL", "FYZ", "ZMU", "MDO", "ANJ", " ", " ", " "},
-    //         {" ", "ZMU", "ZMU", "OPS", "OPS", "WEB", "WEB", " ", " ", " ", " "},
-    //         {" ", "POP", "POP", "OVS", "FYZ", "MAT", "MAT", " ", " ", " ", " "},
-    //     };
-    //     return rozvrh;
+    // String rozvrh[][] = {
+    // {" ", "HW", "HW", "NET", "NET", "PCV", "PCV", " ", " ANJ", "ANJ", " "},
+    // {" ", "MAT", "ANJ", "PRG", "PRG", "CJL", "MAT", " ", " ", " ", " "},
+    // {" ", "TEV", "TEV", "CJL", "FYZ", "ZMU", "MDO", "ANJ", " ", " ", " "},
+    // {" ", "ZMU", "ZMU", "OPS", "OPS", "WEB", "WEB", " ", " ", " ", " "},
+    // {" ", "POP", "POP", "OVS", "FYZ", "MAT", "MAT", " ", " ", " ", " "},
+    // };
+    // return rozvrh;
     // }
 
     public static void setup(String name, int nultaH, int pocetHVDnu, Scanner sc, String dny[], String rozvrh[][]) {
-        
+
     }
 
     public static void fillRozvrh(int pocetHVDnu, String rozvrh[][], int day, Scanner sc) {
-        for(int hour = 1; hour <= pocetHVDnu; hour++) {
+        for (int hour = 1; hour <= pocetHVDnu; hour++) {
             System.out.println(hour + ". Hodinu máš jaký predmet?");
             sc.reset();
             rozvrh[day][hour] = sc.nextLine();
@@ -67,42 +65,41 @@ class Rozvrh {
 
     public static void fillInRest(int pocetHVDnu, String rozvrh[][], int day) {
         if (pocetHVDnu < 10) {
-            for(int i = pocetHVDnu+1; i < 10; i++) {
+            for (int i = pocetHVDnu + 1; i < 10; i++) {
                 rozvrh[day][i] = " ";
             }
         }
     }
 
     public static void user(String name, String dny[], int nultaH, String rozvrh[][], Scanner sc, int pocetHVDnu) {
-        System.out.println("Vítej uživateli: " + name + ".\nZacneme Pondelim, mas nultou hodinu? (tohle uvidíš ještě 4x)\n1. Ano | 2. Ne\n----------------");
-            for(int day = 0; day < dny.length; day++) {
-               
-                    
-                System.out.println("Mas v " + (String) dny[day] + " nultou hodinu? 1. Ano | 2. Ne");
+        System.out.println("Vítej uživateli: " + name
+                + ".\nZacneme Pondelim, mas nultou hodinu? (tohle uvidíš ještě 4x)\n1. Ano | 2. Ne\n----------------");
+        for (int day = 0; day < dny.length; day++) {
+
+            System.out.println("Mas v " + (String) dny[day] + " nultou hodinu? 1. Ano | 2. Ne");
+            sc.reset();
+            nultaH = Integer.parseInt(sc.nextLine());
+
+            if (nultaH == 2) {
+
+                rozvrh[day][0] = " ";
+
+            } else {
+
+                System.out.println("Co mas za 0. hodinu?");
                 sc.reset();
-                nultaH = Integer.parseInt(sc.nextLine());
-
-                if(nultaH == 2) {
-
-                    rozvrh[day][0] = " "; 
-
-                } else {
-
-                    System.out.println("Co mas za 0. hodinu?");
-                    sc.reset();
-                    rozvrh[day][0] = sc.nextLine();
-
-                }
-
-                System.out.println("Kolik mas v " + (String) dny[day] + " hodin? (krome 0. hodiny)");
-                sc.reset();
-                pocetHVDnu = Integer.parseInt(sc.nextLine());
-
-
-                fillRozvrh(pocetHVDnu, rozvrh, day, sc);
-                fillInRest(pocetHVDnu, rozvrh, day);
+                rozvrh[day][0] = sc.nextLine();
 
             }
+
+            System.out.println("Kolik mas v " + (String) dny[day] + " hodin? (krome 0. hodiny)");
+            sc.reset();
+            pocetHVDnu = Integer.parseInt(sc.nextLine());
+
+            fillRozvrh(pocetHVDnu, rozvrh, day, sc);
+            fillInRest(pocetHVDnu, rozvrh, day);
+
+        }
     }
 
     public static void searchForPrg(String rozvrh[][], String dny[], String name) {
@@ -111,15 +108,15 @@ class Rozvrh {
         String hodina;
         int incr = 0;
         for (int i = 0; i < 5; i++) {
-            for(int y = 0; y < 10; y++) {
+            for (int y = 0; y < 10; y++) {
                 hodina = rozvrh[i][y];
-                
-                if(hodina.toUpperCase().equals("PRG")) {
+
+                if (hodina.toUpperCase().equals("PRG")) {
                     den[incr] = i;
-                    hodinyPrg[incr] = y; 
+                    hodinyPrg[incr] = y;
                     incr++;
                 }
-                
+
             }
         }
         printFin(den, hodinyPrg, dny, name, incr);
@@ -128,7 +125,7 @@ class Rozvrh {
 
     public static void printFin(int den[], int hodinyPrg[], String dny[], String name, int incr) {
         System.out.println("Studente: " + name + "\nHodiny programovani mas v: ");
-        for(int i = 0; i < incr; i++) {
+        for (int i = 0; i < incr; i++) {
             System.out.println(dny[den[i]] + ": " + hodinyPrg[i] + ". hodinu");
         }
 
